682b8d80dca4d26ea30c76e220f97261579f01d0
Maxime <maxime.jme@gmail.com>
2020-06-21 02:47:20 +0200

    update the language picker with gsap

--- pages/index.vue
+++ pages/index.vue
@@ -12,12 +12,10 @@
 	</div>
 </template>
 
-<script lang="ts">
-	import Vue from 'vue'
-
-	export default Vue.extend({
-
-	})
+<script>
+	export default {
+		name: "index"
+	}
 </script>
 
 <style lang="scss">
